80fab2b17ca4f19c927c24035aa0e4fd3d4f1712
Yurii Zubar <barsuk0831@gmail.com>
2020-09-21 20:51:29 +0300

    fix code

--- js/task-6.js
+++ js/task-6.js
@@ -1,7 +1,8 @@
 const inputEl = document.querySelector("#validation-input");
-const inputBlur = (event) =>
-  event.currentTarget.value.length !== 6
-    ? inputEl.classList.add("invalid")
-    : inputEl.classList.replace("invalid", "valid");
+const inputBlur = (event) => {
+  if (event.currentTarget.value.length !== 6) {
+    inputEl.classList.add("invalid");
+  } else inputEl.classList.replace("invalid", "valid");
+};
 
 inputEl.addEventListener("blur", inputBlur);
