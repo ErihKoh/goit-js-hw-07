a0c038921b495f5de15550468bade4ccec816236
Yurii Zubar <barsuk0831@gmail.com>
2020-09-21 18:22:34 +0300

    йцук

---
+++ js/task-8.js
@@ -0,0 +1,17 @@
+// const inputEl = document.querySelector("#controls");
+// const boxesEl = document.querySelector("#boxes");
+// const createBoxes = (amount) => {
+//   for (let i = 1; i <= amount; i += 1) {
+//     const boxEl = document.createElement("div");
+
+//     boxesEl.appendChild(boxEl);
+//   }
+//   return boxesEl;
+// };
+
+// let quantity = 0;
+
+// inputEl.addEventListener("input", createBoxes);
+// console.log(document);
+
+// сделаю завтра
